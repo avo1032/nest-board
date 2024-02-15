98456381d316378cc21505833aafe3c39b4b5f95
avo1032 <swan1032@naver.com>
2024-02-16 01:41:26 +0900

    feat: 스웨거 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardController } from './board/board.controller';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [BoardModule],
+  controllers: [AppController, BoardController],
   providers: [AppService],
 })
 export class AppModule {}
